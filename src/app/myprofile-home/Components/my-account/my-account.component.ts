4e8f01dd7fdfd4bf152d6903dc8e80313a82b547
SLTC-GroupProject <57896573+SLTC-GroupProject@users.noreply.github.com>
2020-01-06 11:58:46 +0530

    added functions to my account component

--- src/app/myprofile-home/Components/my-account/my-account.component.ts
+++ src/app/myprofile-home/Components/my-account/my-account.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileService } from '../../profile.service';
+import { UserService } from 'src/app/user/Sahred/user.service';
 
 @Component({
   selector: 'app-my-account',
@@ -8,17 +9,39 @@ import { ProfileService } from '../../profile.service';
 })
 export class MyAccountComponent implements OnInit {
 
-  constructor(private profileService: ProfileService) { }
+  constructor(private profileService: ProfileService, private userService: UserService) { }
 
   adDetails: any;
   filteredAdDetails: any;
 
+  userDetails = {
+    fullName: "",
+    email: "",
+    userName: "",
+    role: "",
+    userId: ""
+  };
+
   ngOnInit() {
-    this.getAllAdvertisements();
+    
+
+    this.userService.getUserProfile().subscribe(
+      res => {
+        this.userDetails.email = res["email"];
+        this.userDetails.fullName = res["fullName"];
+        this.userDetails.userName = res["userName"];
+        this.userDetails.role = res["role"];
+        this.userDetails.userId = res["id"]
+        this.getAllAdvertisements();
+      },
+      err => {
+        console.log(err);
+      },
+      );
   }
 
   getAllAdvertisements(){
-    this.profileService.getAdvertisementByUser('123456')
+    this.profileService.getAdvertisementByUser("123456")
       .subscribe((data: any) => {
         var xx =data;
         this.adDetails = data;
